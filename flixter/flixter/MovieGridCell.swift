afe0d2619ad58dc98d2f7d7b9ccb1f07cea91130
fatjeta <93055082+fatjet@users.noreply.github.com>
2022-02-18 23:11:54 -0500

    Add files via upload

---
+++ flixter/flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Fatjeta Ukaj on 2/18/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
